094004da1d4d64c8d6d930c75b822350c6fc95ee
aqwarius2003 <00victorr@gmail.com>
2024-05-21 19:12:24 +0400

    переименование переменных, форматирование скрипта, правки багов

--- main.py
+++ main.py
@@ -1,52 +1,55 @@
 import pandas as pd
 import datetime
 from collections import defaultdict
-
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 foundation_date = 1920
 
-df = pd.read_excel(io='wine.xlsx', sheet_name='Лист1', na_values=' ', keep_default_na=False)
-wines_dic = df.to_dict(orient='records')
 
-alco_in_category = defaultdict(list)
-
-for wine in wines_dic:
-    category = wine["Категория"]
-    alco_in_category[category].append(wine)
-
-
-def age_word(age):
+def word_age(age):
     diglast = age % 10
-    if age in range(5, 20):
+    if (age % 100) in range(11, 15):
         return 'лет'
-    elif 1 in (age, diglast):
+    elif diglast == 1:
         return 'год'
-    elif {age, diglast} & {2, 3, 4}:
+    elif diglast in [2, 3, 4]:
         return 'года'
     else:
         return 'лет'
 
 
-current_year = datetime.datetime.now().year
-age = current_year - foundation_date
+def main():
+    df = pd.read_excel(io='wine.xlsx', sheet_name='Лист1', na_values=' ', keep_default_na=False)
+    wines = df.to_dict(orient='records')
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+    wine_categories = defaultdict(list)
 
-template = env.get_template('template.html')
+    for wine in wines:
+        category = wine["Категория"]
+        wine_categories[category].append(wine)
 
-rendered_page = template.render(
-    age=f'{age} age_word(age)',
-    alco_in_category=alco_in_category
-)
+    current_year = datetime.datetime.now().year
+    age = current_year - foundation_date
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(
+        age=f'{age} {word_age(age)}',
+        wine_categories=wine_categories
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
